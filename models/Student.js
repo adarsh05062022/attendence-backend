aced3c3b6b761dc054e3b6bd548911992d0f099f
Adarsh Dubey <106887970+adarsh05062022@users.noreply.github.com>
2024-05-05 07:20:51 +0530

    Add files via upload

---
+++ models/Student.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const studentSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  section: { type: String, required: true },
+  roll:{type:String,required:true},
+  password:{type:String,required:true}
+  // Other relevant fields
+});
+
+const Student = mongoose.model('Student', studentSchema);
+export default Student;
---
+++ models/Teacher.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const teacherSchema =new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+
+const Teacher  = mongoose.model("Teacher",teacherSchema);
+
+
+export default Teacher;
